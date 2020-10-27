6b33226fa6c5068c6c8fe42f7c40574ac36b5119
caiosignor <caiosignor@gmail.com>
2020-10-27 20:34:22 -0300

    working with lv_drivers flush callback

--- lib/lvgl/lv.c
+++ lib/lvgl/lv.c
@@ -4,31 +4,60 @@
 #include "main.h"
 #include "gpio.h"
 #include "usart.h"
+
 #define BASE_TEMPO 5 //ms
 
 uint32_t tempo;
 
-//callback para copiar o buffer para uma área específica do display.
-void my_flush_cb(lv_disp_drv_t *disp_drv, const lv_area_t *area, lv_color_t *color_p)
+void ili9341_flush(lv_disp_drv_t *drv, const lv_area_t *area, const lv_color_t *color_p)
 {
-    /*The most simple case (but also the slowest) to put all pixels to the screen one-by-one*/
-    int32_t x, y;
-    for (y = area->y1; y <= area->y2; y++)
+    if (area->x2 < 0 || area->y2 < 0 || area->x1 > (LV_HOR_RES_MAX - 1) || area->y1 > (LV_VER_RES_MAX - 1))
     {
-        for (x = area->x1; x <= area->x2; x++)
-        {
-            ILI9341_Draw_Pixel(x, y, color_p->ch.blue);
-            // ILI9341_Draw_Pixel(x, y, color_p->ch.blue + color_p->ch.red + color_p->ch.green_h + color_p->ch.green_l);
-            // ILI9341_Draw_Pixel(x, y, color_p->ch.blue + color_p->ch.red + color_p->ch.green);
-            // ILI9341_Draw_Pixel(x, y, color_p->ch.blue + color_p->ch.red + color_p->ch.green);
-            // put_px(x, y, *color_p)
-            color_p++;
-        }
+        lv_disp_flush_ready(drv);
+        return;
     }
 
-    /* IMPORTANT!!!
-     * Inform the graphics library that you are ready with the flushing*/
-    lv_disp_flush_ready(disp_drv);
+    /* Truncate the area to the screen */
+    int32_t act_x1 = area->x1 < 0 ? 0 : area->x1;
+    int32_t act_y1 = area->y1 < 0 ? 0 : area->y1;
+    int32_t act_x2 = area->x2 > LV_HOR_RES_MAX - 1 ? LV_HOR_RES_MAX - 1 : area->x2;
+    int32_t act_y2 = area->y2 > LV_VER_RES_MAX - 1 ? LV_VER_RES_MAX - 1 : area->y2;
+
+    int32_t y;
+    uint8_t data[4];
+    int32_t len = len = (act_x2 - act_x1 + 1) * 2;
+    lv_coord_t w = (area->x2 - area->x1) + 1;
+
+    /* window horizontal */
+    ILI9341_Write_Command(ILI9341_CASET);
+    // ili9341_write(ILI9341_CMD_MODE, ILI9341_CASET);
+    data[0] = act_x1 >> 8;
+    data[1] = act_x1;
+    data[2] = act_x2 >> 8;
+    data[3] = act_x2;
+    // ili9341_write_array(ILI9341_DATA_MODE, data, 4);
+    ILI9341_Write_Array(data, 4);
+
+    /* window vertical */
+    ILI9341_Write_Command(ILI9341_PASET);
+    // ili9341_write(ILI9341_CMD_MODE, ILI9341_PASET);
+    data[0] = act_y1 >> 8;
+    data[1] = act_y1;
+    data[2] = act_y2 >> 8;
+    data[3] = act_y2;
+    // ili9341_write_array(ILI9341_DATA_MODE, data, 4);
+    ILI9341_Write_Array(data, 4);
+
+    ILI9341_Write_Command(ILI9341_RAMWR);
+    // ili9341_write(ILI9341_CMD_MODE, ILI9341_RAMWR);
+    for (y = act_y1; y <= act_y2; y++)
+    {
+        ILI9341_Write_Array((uint8_t *)color_p, len);
+        // ili9341_write_array(ILI9341_DATA_MODE, (uint8_t *)color_p, len);
+        color_p += w;
+    }
+
+    lv_disp_flush_ready(drv);
 }
 
 void lv_ex_btn_1(void)
@@ -64,8 +93,8 @@ void lv_setup()
 
     //configura o driver do display;
     disp_drv.buffer = &disp_buff;
-    disp_drv.flush_cb = my_flush_cb;
-    // disp_drv.flush_cb = ili9341_flush;
+    // disp_drv.flush_cb = my_flush_cb;
+    disp_drv.flush_cb = ili9341_flush;
 
     //registra o driver do display no objeto do display.
     lv_disp_drv_register(&disp_drv);
